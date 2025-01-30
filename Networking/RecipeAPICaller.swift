e7c5e4187ac6eae112cf8077b88ab93047402994
Kabir Dhillon <kabirdhillon99@gmail.com>
2025-01-29 21:36:24 -0800

    RecipeAPICaller
    - Added an API Caller struct to handle networking call
    - Added documentation comments for Recipe Endpoints

---
+++ Networking/RecipeAPICaller.swift
@@ -0,0 +1,22 @@
+//
+//  RecipeAPICaller.swift
+//  Recipes
+//
+//  Created by Kabir Dhillon on 1/29/25.
+//
+
+import Foundation
+
+/// A struct responsible for calling the API to fetch recipe data
+struct RecipeAPICaller {
+    
+    /// Fetches recipe data from the API
+    func fetchRecipeData<T: Codable>(urlString: String, decoderType: T.Type) async throws -> T {
+        guard let url = URL(string: urlString) else {
+            throw URLError(.badURL)
+        }
+        
+        let (data, _) = try await URLSession.shared.data(from: url)
+        return try JSONDecoder().decode(decoderType.self, from: data)
+    }
+}
