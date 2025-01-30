3688eaf59838bfec321f14997900282cc851aaa2
Kabir Dhillon <kabirdhillon99@gmail.com>
2025-01-30 15:57:31 -0800

    Async Image View
    - Created an async image view to handle retrieving an image from a url string

---
+++ Views/AsyncImageView/AsyncImageView.swift
@@ -0,0 +1,38 @@
+//
+//  AsyncImageView.swift
+//  Recipes
+//
+//  Created by Kabir Dhillon on 1/30/25.
+//
+
+import SwiftUI
+
+/// A View that represents an asynchronously loads and displays an image
+struct AsyncImageView: View {
+    
+    // MARK: - Properties
+    @State private var viewModel = ViewModel()
+    @State private var image: UIImage? = nil
+    let urlString: String
+    
+    // MARK: - Views
+    
+    var body: some View {
+        Group {
+            if let image = image {
+                Image(uiImage: image)
+                    .resizable()
+                    .scaledToFit()
+            } else {
+                ProgressView()
+            }
+        }
+        .task {
+            image = await viewModel.getImage(nameString: urlString)
+        }
+    }
+}
+
+#Preview {
+    AsyncImageView(urlString: "https://d3jbb8n5wk0qxi.cloudfront.net/photos/b9ab0071-b281-4bee-b361-ec340d405320/large.jpg")
+}
