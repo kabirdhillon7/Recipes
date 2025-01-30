34ffcd93f45aefec6db67d3de28c7b9feb915d3c
Kabir Dhillon <kabirdhillon99@gmail.com>
2025-01-29 20:43:21 -0800

    Initial commit

---
+++ RecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipesApp.swift
+//  Recipes
+//
+//  Created by Kabir Dhillon on 1/29/25.
+//
+
+import SwiftUI
+
+@main
+struct RecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
