c527843b7ba2431012c1eeb04be44de09a843aeb
Kabir Dhillon <kabirdhillon99@gmail.com>
2025-01-29 22:21:02 -0800

    Swift Data Set-up

--- RecipesApp.swift
+++ RecipesApp.swift
@@ -5,13 +5,26 @@
 //  Created by Kabir Dhillon on 1/29/25.
 //
 
+import SwiftData
 import SwiftUI
 
 @main
 struct RecipesApp: App {
+    
+    @State private var container: ModelContainer
+    
+    init() {
+        do {
+            let config = ModelConfiguration (for: Recipe.self)
+            container = try ModelContainer(for: Recipe.self, configurations: config)
+        } catch {
+            fatalError("Failed to set up the model container: \(error)")
+        }
+    }
     var body: some Scene {
         WindowGroup {
-            RecipeListView()
+            RecipeListView(modelContext: container.mainContext)
+                .modelContainer(for: Recipe.self)
         }
     }
 }
